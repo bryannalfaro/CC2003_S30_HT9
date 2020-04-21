9ff7a036a5e44425ee8624139e349c718de9ae5d
Bryann Alfaro <alf19372@uvg.edu.gt>
2020-04-21 12:54:59 -0600

    archivos

--- src/principal.java
+++ src/principal.java
@@ -6,12 +6,23 @@ public class principal {
 		// TODO Auto-generated method stub
 		
 		Scanner entrada = new Scanner(System.in);
+		FactoryTree factory  = new FactoryTree();
+		
 		
 		System.out.println("Seleccione la opcion a utilizar: ");
 		System.out.println("1. HashMap ");
 		System.out.println("2. SplayTree ");
 		
 		int opcion= entrada.nextInt();
+		
+		if(opcion==1) {
+			
+			//HashMap 
+			
+		}else if (opcion==2) {
+			//SplayTree
+			
+		}
 
 	}
 
