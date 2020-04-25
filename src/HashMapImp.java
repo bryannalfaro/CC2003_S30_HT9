e991f6cb767571f671ae59169803fdce29ad9190
arr19422 <arr19422@uvg.edu.gt>
2020-04-25 11:07:20 -0600

    Cambios al principal, contiene error

--- src/HashMapImp.java
+++ src/HashMapImp.java
@@ -5,7 +5,7 @@ public class HashMapImp<K,V> implements Map<K,V> {
 	HashMap<K,V> mapa = new HashMap<K,V>();
 	
 	public void put (K key, V value) {
-		
+		mapa.put(key, value);
 	}
 	
 	public V get(K key) {
--- src/principal.java
+++ src/principal.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.Map;
 import java.util.Scanner;
 
 public class principal {
@@ -7,7 +11,7 @@ public class principal {
 		
 		Scanner entrada = new Scanner(System.in);
 		FactoryTree factory  = new FactoryTree();
-		
+		Map<String, String> map = null;
 		
 		System.out.println("Seleccione la opcion a utilizar: ");
 		System.out.println("1. HashMap ");
@@ -16,7 +20,19 @@ public class principal {
 		int opcion= entrada.nextInt();
 		
 		if(opcion==1) {
-			
+			map = factory.getFactoryMap(1);
+			String directorioNuevo = System.getProperty("user.dir");
+            Scanner inputScan = new Scanner(new File(directorioNuevo + "\\Spanish.txt"));
+            while (inputScan.hasNextLine()) {
+                String line = inputScan.nextLine();
+                int indice_particion = line.lastIndexOf("	");
+                String primera_parte = line.substring(0,indice_particion).trim();
+               
+                String segunda_parte = line.substring(indice_particion+1).trim();
+                segunda_parte = segunda_parte.split(",")[1];
+                System.out.println(segunda_parte);
+                map.put(primera_parte,segunda_parte);
+            }
 			//HashMap 
 			
 		}else if (opcion==2) {
